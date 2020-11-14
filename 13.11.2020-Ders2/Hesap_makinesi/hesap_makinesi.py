54a216e8b2bc773afd3ea8fed0662ab523587685
Mine ÇAKIR <152120181058@ogrenci.ogu.edu.tr>
2020-11-14 13:12:34 +0300

    hesap_makinesi kodu eklendi.

---
+++ 13.11.2020-Ders2/Hesap_makinesi/hesap_makinesi.py
@@ -0,0 +1,19 @@
+# Hesap makinesi
+print("Enter 1 for addition")
+print("Enter 2 for subtact")
+print("Enter 3 for division")
+print("---------------------------------")
+first=int(input("Enter a first number:"))
+second=int(input("Enter a second number:"))
+secenek=int(input("Seceneğinizi giriniz: "))
+if(secenek==1):
+    sum=first+second
+    print("Sum:",sum)
+
+elif(secenek==2):
+    subt=first-second
+    print(subt)
+    
+elif(secenek==3):
+    div=first/second
+    print(div)
