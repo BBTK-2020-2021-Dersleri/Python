f13d5eb80982b65d44ee442d8f66f00d339b504c
Mine ÇAKIR <152120181058@ogrenci.ogu.edu.tr>
2020-12-19 14:13:59 +0300

    PrimeNumber kodu düzeltildi.

--- Ders5-11.12.2020/Example3-isPrimeNumber.py
+++ Ders5-11.12.2020/Example3-isPrimeNumber.py
@@ -1,11 +1,11 @@
 number = int(input("Enter a number: "))
 def isPrime(number):
+    if(number==1):
+        return False
     for i in range (2,number):
         if(number%i==0):
             return False
-        else:
-            return False
-        return True
+    return True
     
 if(isPrime(number)):
     print("Prime Number.")
